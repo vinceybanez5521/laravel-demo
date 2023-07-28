e9074d5ba42882fc6b3acf8c89c13103f18116bc
vinceybanez5521 <vinceybanez5521@gmail.com>
2023-07-28 17:48:31 +0800

    Add Repository Design Pattern

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,19 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use App\Repositories\EmployeeRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class EmployeeController extends Controller
 {
 
-    public function __construct()
+    /* public function __construct()
     {
         $this->middleware('auth', ['except' => ['index', 'show']]);
+    } */
+
+    private $employeeRepository;
+
+    public function __construct(EmployeeRepository $employeeRepository)
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->employeeRepository = $employeeRepository;
     }
 
     public function index()
     {
+        /*
         $data = Employee::all(); // Eloquent (Object Relational Mapping or ORM for Laravel)
         $data = Employee::where('age', '>', 0)->get();
         $data = Employee::where('first_name', 'LIKE', 'j%')->get();
@@ -33,23 +43,30 @@ class EmployeeController extends Controller
         $data = Employee::paginate(10);
         
         // dd($data);
+        */
+
+        $data = $this->employeeRepository->all();
         return view("employee.index", ['employees' => $data]);
     }
 
     public function show($id)
     {
+        /*
         $data = Employee::where('id', $id)->first();
         $data = Employee::findOrFail($id);
         // $data->full_name = "John";
         // dd($data);
+        */
+        $data = $this->employeeRepository->findById($id);
         return view("employee.show", ['employee' => $data]);
     }
 
-    public function create() {
+    public function create()
+    {
         return view('employee.create');
     }
 
-    public function store(Request $request) {
+    /* public function store(Request $request) {
         $validated = $request->validate([
             'first_name' => ['required'],
             'last_name' => ['required'],
@@ -58,15 +75,22 @@ class EmployeeController extends Controller
 
         Employee::create($validated);
         return redirect()->route('employee.index');
+    } */
+
+    public function store()
+    {
+        $this->employeeRepository->store();
+        return redirect()->route('employee.index');
     }
 
     public function edit($id)
     {
-        $data = Employee::findOrFail($id);
+        // $data = Employee::findOrFail($id);
+        $data = $this->employeeRepository->findById($id);
         return view('employee.edit', ['employee' => $data]);
     }
 
-    public function update(Request $request, Employee $employee)
+    /* public function update(Request $request, Employee $employee)
     {
         $validated = $request->validate([
             'first_name' => ['required'],
@@ -76,10 +100,23 @@ class EmployeeController extends Controller
 
         $employee->update($validated);
         return redirect()->route('employee.show', ['id' => $employee->id]);
+    } */
+
+    public function update($id)
+    {
+        $this->employeeRepository->update($id);
+        return redirect()->route('employee.show', ['id' => $id]);
     }
 
-    public function destroy(Employee $employee) {
+    /* public function destroy(Employee $employee)
+    {
         $employee->delete();
+        return redirect()->route('employee.index');
+    } */
+
+    public function destroy($id)
+    {
+        $this->employeeRepository->delete($id);
         return redirect()->route('employee.index');
     }
 }
---
+++ app/Repositories/EmployeeRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Employee;
+
+class EmployeeRepository
+{
+
+    public function all()
+    {
+        return Employee::paginate(10);
+    }
+
+    public function findById($id)
+    {
+        return Employee::findOrFail($id);
+    }
+
+    public function update($id)
+    {
+        $validated = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required'],
+        ]);
+
+        $employee = $this->findById($id);
+        return $employee->update($validated);
+    }
+
+    public function store()
+    {
+        $validated = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required'],
+        ]);
+
+        return Employee::create($validated);
+    }
+
+    public function delete($id)
+    {
+        return Employee::destroy($id);
+    }
+}
